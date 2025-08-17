afb21ccb9404c4774c463081a189f5aec605079b
stephane <stephane.betton@gmail.com>
2025-08-17 11:40:09 +0200

    Ajout de la recherche et des filtres dans la liste des enfants :
    - **Mise à jour des dépendances** : Ajout des librairies PrimeFlex et TailwindCSS-PrimeUI.
    - **Personnalisation de l'interface** :
      - Ajout de champs de recherche interactifs avec des boutons pour effacer les filtres.
      - Refonte de la colonne "Actions" avec des icônes pour modifier et supprimer.
    - **Amélioration des styles** : Intégration de nouveaux fichiers CSS (TailwindCSS et PrimeFlex) pour une meilleure mise en page.
    - **Fonctionnalités utilisateurs** : Ajout de la suppression directe d'un enfant avec actualisation de la table.

--- enfant/src/lib/enfant/liste/liste.ts
+++ enfant/src/lib/enfant/liste/liste.ts
@@ -1,22 +1,29 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  CUSTOM_ELEMENTS_SCHEMA,
-  inject,
-  signal,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component, CUSTOM_ELEMENTS_SCHEMA, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DialogService } from 'primeng/dynamicdialog';
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { Enfant } from '../model/enfant';
 import { DialogModule } from 'primeng/dialog';
 import { Router } from '@angular/router';
 import { EnfantStore } from '@assistante-maternelle/core';
+import { TooltipModule } from 'primeng/tooltip';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'enfant-liste',
-  imports: [CommonModule, TableModule, ButtonModule, DialogModule],
+  imports: [
+    CommonModule,
+    TableModule,
+    ButtonModule,
+    DialogModule,
+    TooltipModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+  ],
   providers: [DialogService],
   schemas: [CUSTOM_ELEMENTS_SCHEMA], // Ajout pour supporter les Web Components
   templateUrl: './liste.html',
@@ -28,11 +35,20 @@ export class Liste {
   title = signal<string | null>('Modifier');
   router = inject(Router);
   enfants = this._store.enfants;
-
+  searchValue: string | undefined;
   navigateToFormulaire(title: string, enfant?: Enfant) {
     this._store.selectEnfant(enfant?.id);
     this.router.navigate(['/formulaire'], {
       queryParams: { title }, // Utilisez des queryParams
     });
+  }
+
+  supprimerUnEnfant(enfantId: string) {
+    this._store.supprimerEnfant(enfantId);
+  }
+
+  clear(table: Table) {
+    table.clear();
+    this.searchValue = '';
   }
 }
